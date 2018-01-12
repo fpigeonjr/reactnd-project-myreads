4684dcf6e0380684ef783f741bf8a4a8f4cc8ac1
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-11 21:26:09 -0600

    refactors to pass down filtered books to BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import * as BooksAPI from "./BooksAPI";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
 import BookShelf from "./BookShelf";
 import SearchBooks from "./SearchBooks";
@@ -26,52 +26,62 @@ class BooksApp extends Component {
   }
 
   render() {
+    const FourOhFour = () => <p>Nothing to see here.</p>;
     return (
       <div className="app">
-        <Route
-          path="/search"
-          render={() => (
-            <SearchBooks
-              books={this.state.books}
-              updateBooks={this.updateBooks}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/"
-          render={({ history }) => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <div className="list-books-content">
-                <BookShelf
-                  title="Currenty Reading"
-                  books={this.state.books}
-                  shelf="currentlyReading"
-                  updateBooks={this.updateBooks}
-                />
-                <BookShelf
-                  title="Want To Read"
-                  books={this.state.books}
-                  shelf="wantToRead"
-                  updateBooks={this.updateBooks}
-                />
+        <Switch>
+          <Route
+            path="/search"
+            render={() => (
+              <SearchBooks
+                books={this.state.books}
+                updateBooks={this.updateBooks}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/"
+            render={({ history }) => (
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
 
-                <BookShelf
-                  title="Read"
-                  books={this.state.books}
-                  shelf="read"
-                  updateBooks={this.updateBooks}
-                />
+                <div className="list-books-content">
+                  <BookShelf
+                    title="Currenty Reading"
+                    books={this.state.books.filter(
+                      book => "currentlyReading" === book.shelf
+                    )}
+                    updateBooks={this.updateBooks}
+                  />
+
+                  <BookShelf
+                    title="Want To Read"
+                    books={this.state.books.filter(
+                      book => "wantToRead" === book.shelf
+                    )}
+                    updateBooks={this.updateBooks}
+                  />
+
+                  <BookShelf
+                    title="Read"
+                    books={this.state.books.filter(
+                      book => "read" === book.shelf
+                    )}
+                    updateBooks={this.updateBooks}
+                  />
+                </div>
+
+                <div className="open-search">
+                  <Link to="/search">Add a book</Link>
+                </div>
               </div>
-              <div className="open-search">
-                <Link to="/search">Add a book</Link>
-              </div>
-            </div>
-          )}
-        />
+            )}
+          />
+          <Route component={FourOhFour} />
+        </Switch>
       </div>
     );
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -6,11 +6,9 @@ const BookShelf = props => (
     <h2 className="bookshelf-title">{props.title}</h2>
     <div className="bookshelf-books">
       <ol className="books-grid">
-        {props.books
-          .filter(book => props.shelf === book.shelf)
-          .map(book => (
-            <Book key={book.id} book={book} updateBooks={props.updateBooks} />
-          ))}
+        {props.books.map(book => (
+          <Book key={book.id} book={book} updateBooks={props.updateBooks} />
+        ))}
       </ol>
     </div>
   </div>

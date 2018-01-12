14c51784c5d529ddf7cdb9f981189c0f24b3ed66
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-01-11 21:06:26 -0600

    converts this component into a stateless functional component as it does not have any state or lifecycle methods

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,19 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 import Book from "./Book";
 
-class BookShelf extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    updateBooks: PropTypes.func.isRequired
-  };
-
-  render() {
-    const { books, title, shelf } = this.props;
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books
-              .filter(book => shelf === book.shelf)
-              .map(book => (
-                <Book
-                  key={book.id}
-                  book={book}
-                  updateBooks={this.props.updateBooks}
-                />
-              ))}
-          </ol>
-        </div>
-      </div>
-    );
-  }
-}
+const BookShelf = props => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{props.title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.books
+          .filter(book => props.shelf === book.shelf)
+          .map(book => (
+            <Book key={book.id} book={book} updateBooks={props.updateBooks} />
+          ))}
+      </ol>
+    </div>
+  </div>
+);
 
 export default BookShelf;
